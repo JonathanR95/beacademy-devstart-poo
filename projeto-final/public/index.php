ac1100369ef484a77842fd8054442f49b7c4c33b
Jonathan Rodrigues <jonathan.rf.sousa@gmail.com>
2022-06-07 14:36:21 -0300

    adição do controler

--- projeto-final/public/index.php
+++ projeto-final/public/index.php
@@ -1,3 +1,13 @@
 <?php
 
-echo "Olá mundo";
+ini_set('display_errors',1);
+
+include '../vendor/autoload.php';
+
+use App\Controller\IndexController;
+
+$c = new IndexController();
+//$c-> indexAction();
+$c -> loginAction();
+
+//echo "Olá mundo";
---
+++ projeto-final/src/Controller/IndexController.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+class IndexController
+{
+    public function indexAction():void
+    {
+        
+        include dirname(__DIR__).'/View/index/index.php';
+    }
+
+    public function loginAction():void
+    {
+        include dirname(__DIR__).'/view/index/login.php';
+    }
+}
